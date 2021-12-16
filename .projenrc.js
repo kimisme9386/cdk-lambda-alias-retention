2cf6c5f5d87f6dbbe3e4d10266b03e187a0af71e
Chris Yang <kimisme9386@gmail.com>
2021-12-16 15:33:33 +0800

    feat: add config in .projenrc.js

--- .projenrc.js
+++ .projenrc.js
@@ -1,16 +1,98 @@
-const { awscdk } = require('projen');
+const { awscdk, DevEnvironmentDockerImage, Gitpod } = require('projen');
+
+const AUTOMATION_TOKEN = 'PROJEN_GITHUB_TOKEN';
+
 const project = new awscdk.AwsCdkConstructLibrary({
   author: 'Chris Yang',
   authorAddress: 'kimisme9386@gmail.com',
-  cdkVersion: '2.1.0',
+  cdkVersion: '2.0.0',
   defaultReleaseBranch: 'main',
   name: 'cdk-lambda-alias-retention',
   repositoryUrl: 'https://github.com/kimisme9386/cdk-lambda-alias-retention.git',
-
-  // deps: [],                /* Runtime dependencies of this module. */
-  // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
-  // devDeps: [],             /* Build dependencies for this module. */
-  // packageName: undefined,  /* The "name" in package.json. */
-  // release: undefined,      /* Add release management to this project. */
+  keywords: [
+    'lambda',
+    'alias',
+    'retention',
+  ],
+  /**
+   * we default release the main branch(cdkv2) with major version 2.
+   */
+  majorVersion: 2,
+  defaultReleaseBranch: 'main',
+  /**
+    * we also release the cdkv1 branch with major version 1.
+    */
+  releaseBranches: {
+    cdkv1: { npmDistTag: 'cdkv1', majorVersion: 1 },
+  },
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      secret: AUTOMATION_TOKEN,
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['kimisme9386-bot'],
+  },
+  defaultReleaseBranch: 'main',
+  catalog: {
+    twitter: 'ChrisYang9386',
+    announce: false,
+  },
+  workflowNodeVersion: '14.17.0',
+  python: {
+    distName: 'cdk-lambda-alias-retention',
+    module: 'cdk_lambda_alias_retention',
+  },
 });
+
+project.package.addField('resolutions', {
+  'pac-resolver': '^5.0.0',
+  'set-value': '^4.0.1',
+  'ansi-regex': '^5.0.1',
+});
+
+const gitpodPrebuild = project.addTask('gitpod:prebuild', {
+  description: 'Prebuild setup for Gitpod',
+});
+// install and compile only, do not test or package.
+gitpodPrebuild.exec('yarn install --frozen-lockfile --check-files');
+gitpodPrebuild.exec('npx projen compile');
+
+let gitpod = new Gitpod(project, {
+  dockerImage: DevEnvironmentDockerImage.fromFile('.gitpod.Dockerfile'),
+  prebuilds: {
+    addCheck: true,
+    addBadge: true,
+    addLabel: true,
+    branches: true,
+    pullRequests: true,
+    pullRequestsFromForks: true,
+  },
+});
+
+gitpod.addCustomTask({
+  name: 'install package and check zsh and zsh plugin',
+  init: `yarn gitpod:prebuild
+sudo chmod +x ./.gitpod/oh-my-zsh.sh && ./.gitpod/oh-my-zsh.sh`,
+  command: 'npx projen upgrade',
+});
+
+gitpod.addCustomTask({
+  name: 'change default shell to zsh and start zsh shell',
+  command: 'sudo chsh -s $(which zsh) && zsh',
+});
+
+gitpod.addVscodeExtensions(
+  'dbaeumer.vscode-eslint',
+  'ms-azuretools.vscode-docker',
+  'AmazonWebServices.aws-toolkit-vscode',
+);
+
+const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log'];
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
+
 project.synth();
